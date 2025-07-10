a74cab0dc019cfa2f2195d566a21061ffbf8eb72
Maurice Rahme <mrahme97@gmail.com>
2025-07-10 14:10:48 -0400

    fixup! debug

--- source/arm_lib/arm_lib/tasks/manager_based/arm_lib/arm_lib_reach_env.py
+++ source/arm_lib/arm_lib/tasks/manager_based/arm_lib/arm_lib_reach_env.py
@@ -37,6 +37,7 @@ class FrankaSceneCfg(InteractiveSceneCfg):
         prim_path="{ENV_REGEX_NS}/Shelf",
         spawn=sim_utils.CuboidCfg(
             size=(0.5, 0.4, 0.25),
+            collision=True,  # This is the fix
             visual_material=materials.PreviewSurfaceCfg(diffuse_color=(0.3, 0.3, 0.3)),
             collision_props=schemas.CollisionPropertiesCfg(),
             rigid_props=schemas.RigidBodyPropertiesCfg(),
