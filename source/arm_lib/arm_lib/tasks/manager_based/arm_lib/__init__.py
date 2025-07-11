879b28c1d346b95347a27db495626ca951ee069d
Maurice Rahme <mrahme97@gmail.com>
2025-07-11 15:52:14 -0400

    fixup! fixup! fixup! fixup! debug

--- source/arm_lib/arm_lib/tasks/manager_based/arm_lib/__init__.py
+++ source/arm_lib/arm_lib/tasks/manager_based/arm_lib/__init__.py
@@ -17,7 +17,7 @@ gym.register(
     entry_point="arm_lib.tasks.manager_based.arm_lib.franka_reach_env:FrankaReachEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.arm_lib_env_cfg:ArmLibEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.franka_reach_env_cfg:FrankaReachEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
